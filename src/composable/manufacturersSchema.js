fff79875c54955aaf66d9afd8c61ac7fd34b4702
serpents22 <ivandarmawan67@gmail.com>
2023-11-16 15:04:19 +0700

    feat: make variantNotes required

--- src/composable/manufacturersSchema.js
+++ src/composable/manufacturersSchema.js
@@ -2,6 +2,7 @@ import * as yup from 'yup'
 
 export const addVariantSchema = yup.object({
   variant: yup.string().required().label('Variant'),
+  variantNotes: yup.string().required().label('Notes'),
 });
 export const addManufacturerSchema = yup.object({
   name: yup.string().required().label('Name'),
