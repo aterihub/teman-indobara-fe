cc6e079d3bf27844a89dd59de8e22cf7032f1b80
serpents22 <ivandarmawan67@gmail.com>
2023-12-20 16:29:54 +0700

    feat: change styling

--- src/components/button/BaseButton.vue
+++ src/components/button/BaseButton.vue
@@ -1,6 +1,7 @@
 <template>
     <button v-bind="$attrs" :disabled="loading || disable" :class="{'button--loading' : loading}">
       <span class="button__text">{{label}}</span> 
+      <slot></slot>
       <!-- <svg class="spinner" xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M12 22c5.421 0 10-4.579 10-10h-2c0 4.337-3.663 8-8 8s-8-3.663-8-8c0-4.336 3.663-8 8-8V2C6.579 2 2 6.58 2 12c0 5.421 4.579 10 10 10z"></path></svg> -->
     </button>
 </template>
@@ -19,7 +20,7 @@ export default {
 
 <style scoped>
 button {
-  @apply disabled:opacity-75 flex justify-center items-center relative w-full
+  @apply disabled:opacity-75 flex justify-center items-center relative w-full gap-2
           
 }
 .spinner {
