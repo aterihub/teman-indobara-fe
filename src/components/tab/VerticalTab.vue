d406a5759c2655142caf23b51b038500900f52a2
serpents22 <ivandarmawan67@gmail.com>
2023-12-22 09:45:47 +0700

    feat: remove border

--- src/components/tab/VerticalTab.vue
+++ src/components/tab/VerticalTab.vue
@@ -39,12 +39,11 @@ export default {
   @apply 
     items-start justify-start h-full
     flex flex-col w-[200px] gap-2
-    border rounded-lg shadow-inner p-2 bg-white
 }
 
 button {
   @apply disabled:opacity-75 flex items-center relative text-[#353535] cursor-pointer p-2 rounded-md
-  w-full text-[10px] sm:text-[14px] border-transparent border
+  w-full text-[10px] sm:text-[14px] border-2
 } 
 
 button:hover {
@@ -53,7 +52,7 @@ button:hover {
 }
 
 .active {
-  @apply border border-[#C21629]  text-[#C21629]
+  @apply border-2 border-[#C21629]  text-[#C21629]
   transition-colors duration-300 box-border outline-none
 }
 
